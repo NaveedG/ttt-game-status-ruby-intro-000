f9f14578e681d08697772989fe8b5394bd0ff8cb
Naveed Ganjani <nganjani@protonmail.com>
2019-08-23 14:25:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,6 +16,25 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
+    win_index_1 = combo[0]
+    win_index_2 = combo[1]
+    win_index_3 = combo[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return combo
+    end
+  end
+  return false
+end
+
+def won(board)
+  WIN_COMBINATIONS.each do |combo|
       combo.each do |index|
           win_index = combo[index]
           position = board[win_index]
