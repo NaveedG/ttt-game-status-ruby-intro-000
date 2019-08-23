5f7187daeded50ab3814ee12a61f41edec7fefac
Naveed Ganjani <nganjani@protonmail.com>
2019-08-23 15:43:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -48,12 +48,24 @@ def over?(board)
   return true if won?(board) || full?(board) || draw?(board)
 end
 
-def winner(board, token)
-  WIN_COMBINATIONS.each do | combo |
-    combo.each do |index|
-      win_index = combo[index]
-      position = board[index]
-      return token if position == token
+def winner(board)
+  if won?(board)
+    WIN_COMBINATIONS.each do |combo|
+      win_index_1 = combo[0]
+      win_index_2 = combo[1]
+      win_index_3 = combo[2]
+
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return "X"
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return "O"
+      end
     end
+  else
+    return false
   end
 end
